471566e7693da6d63b0d82a3f79464c398ed8f07
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2019-06-05 16:52:16 -0300

    add protobuf and openssl arm64 libs

--- android/libprotobuf/armv7-a/3.7.0/build-protobuf-android.sh
+++ android/libprotobuf/armv7-a/3.7.0/build-protobuf-android.sh
@@ -1,4 +1,4 @@
-export PREFIX=$HOME/dev/android/protobuf-3.6.1/
+export PREFIX=$HOME/dev/android/protobuf-3.7.0/
 export PATH=$HOME/dev/android/arm-16-toolchain-clang/bin:$PATH
 export SYSROOT=$HOME/dev/android/arm-16-toolchain-clang/sysroot
 export CC="arm-linux-androideabi-clang --sysroot $SYSROOT"
@@ -11,8 +11,8 @@ export CXX="arm-linux-androideabi-clang++ --sysroot $SYSROOT"
 --enable-shared \
 --enable-cross-compile \
 --with-protoc=protoc \
-CFLAGS="-march=armv7-a -D__ANDROID_API__=16" \
-CXXFLAGS="-fPIC -frtti -fexceptions -march=armv7-a -D__ANDROID_API__=16 -Os" \
+CFLAGS="-march=armv64-v8 -D__ANDROID_API__=16" \
+CXXFLAGS="-fPIC -frtti -fexceptions -march=arm64-v8 -D__ANDROID_API__=16 -Os" \
 LIBS="-llog -lz -lc++_static"
 
 make -j 4
