c8b488e5dd2aa19f427d16ac646943f2d0e74a1d
Fúlvio Abrahão <fulvio.abrahao@tfgco.com>
2020-01-21 13:55:57 -0300

    Fix build protobuf script.

--- android/libprotobuf/build-android.sh
+++ android/libprotobuf/build-android.sh
@@ -1,9 +1,23 @@
-export PREFIX=$PROTOBUF_PATH
-export PATH=$TOOLCHAIN_PATH/bin:$PATH
-export sysroot=$TOOLCHAIN_PATH/sysroot
-export CC="arm-linux-androideabi-clang --sysroot $SYSROOT"
-export CXX="arm-linux-androideabi-clang++ --sysroot $SYSROOT"
+#!/bin/sh
+./autogen.sh
+rm -rf `pwd`/../libprotobuf
+mkdir -p `pwd`/../libprotobuf/android
+ANDROID64_API_LEVEL="21"
+ANDROID32_API_LEVEL="16"
+
+#You just need to specify your NDK Folder
+export NDK_PATH=/Users/fulvioabrahao/Documents/tfg/github/android-ndk-r20b
+
+export PREFIX=`pwd`
 export build_dir=`pwd`/../libprotobuf/android
+
+$NDK_PATH/build/tools/make-standalone-toolchain.sh --arch=arm --platform=android-$ANDROID64_API_LEVEL --toolchain=arm-linux-android-clang5.0 --install-dir=`pwd`/arm-toolchain-$ANDROID64_API_LEVEL-clang/ --use-llvm --stl=libc++
+$NDK_PATH/build/tools/make-standalone-toolchain.sh --arch=arm --platform=android-$ANDROID32_API_LEVEL --toolchain=arm-linux-android-clang5.0 --install-dir=`pwd`/arm-toolchain-$ANDROID32_API_LEVEL-clang/ --use-llvm --stl=libc++
+
+export PATH=`pwd`/arm-toolchain-$ANDROID32_API_LEVEL-clang/bin:$PATH
+export sysroot=`pwd`/arm-toolchain-$ANDROID32_API_LEVEL-clang/sysroot
+export CC="arm-linux-androideabi-clang --sysroot $sysroot"
+export CXX="arm-linux-androideabi-clang++ --sysroot $sysroot"
 
 
 ./configure \
@@ -13,8 +27,9 @@ export build_dir=`pwd`/../libprotobuf/android
 --disable-shared \
 --prefix="$build_dir/armeabi-v7a" \
 --enable-cross-compile \
-CFLAGS="-march=armv7-a -D__ANDROID_API__=16" \
-CXXFLAGS="-frtti -fexceptions -march=armv7-a -D__ANDROID_API__=16" \
+--verbose \
+CFLAGS="-march=armv7-a -D__ANDROID_API__=$ANDROID32_API_LEVEL" \
+CXXFLAGS="-fPIC -frtti -fexceptions -march=armv7-a -D__ANDROID_API__=$ANDROID32_API_LEVEL" \
 LIBS="-llog -lz -lc++_static"
 
 make -j$(nproc)
@@ -22,15 +37,20 @@ make install
 
 make distclean
 
+export PATH=`pwd`/arm-toolchain-$ANDROID64_API_LEVEL-clang/bin:$PATH
+export sysroot=`pwd`/arm-toolchain-$ANDROID64_API_LEVEL-clang/sysroot
+export CC="aarch64-linux-android$ANDROID64_API_LEVEL-clang --sysroot $sysroot"
+export CXX="aarch64-linux-android$ANDROID64_API_LEVEL-clang++ --sysroot $sysroot"
+
 ./configure \
---host=arm-linux-androideabi \
+--host=aarch64-linux-android \
 --with-protoc=protoc \
 --with-sysroot="$sysroot" \
 --disable-shared \
 --prefix="$build_dir/arm64-v8a" \
 --enable-cross-compile \
-CFLAGS="-march=armv8-a -D__ANDROID_API__=16" \
-CXXFLAGS="-frtti -fexceptions -march=armv8-a -D__ANDROID_API__=16" \
+CFLAGS="-march=armv8-a -D__ANDROID_API__=$ANDROID64_API_LEVEL" \
+CXXFLAGS="-fPIC -frtti -fexceptions -march=armv8-a -D__ANDROID_API__=$ANDROID64_API_LEVEL" \
 LIBS="-llog -lz -lc++_static"
 
 make -j$(nproc)
@@ -45,3 +65,5 @@ rm -rf ../libprotobuf/android/armeabi-v7a
 rm -rf ../libprotobuf/android/arm64-v8a
 
 make distclean
+rm -rf arm-toolchain-$ANDROID64_API_LEVEL-clang
+rm -rf arm-toolchain-$ANDROID32_API_LEVEL-clang
